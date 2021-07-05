019702349fc9a6c4c9500f20e31d246269544804
Cuinardium <sballerini@itba.edu.ar>
2021-07-05 19:27:17 -0300

    Agregado de prototipos esenciales al .h
    se agregaron los prototipos de las funciones esenciales

--- ADT-imdb/imdbADT.h
+++ ADT-imdb/imdbADT.h
@@ -3,5 +3,13 @@
 
 typedef struct imdbCDT * imdbADT;
 
+//devuelve un nuevo TAD vacio
+imdbADT newImdb(imdbADT imdb);
+
+//libera la memoria reservada por el TAD
+void freeImdb(imdbADT imdb);
+
+//agrega la informacion correspondiente al TAD
+void addData(imdbADT imdb, char * data);
 
 #endif //IMDB_ADT_H
